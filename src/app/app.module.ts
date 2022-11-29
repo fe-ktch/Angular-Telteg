bac74db78938d2c164376ebd6928373c1d3b947f
fe-ktch <sangarefantha@gmail.com>
2022-11-29 21:17:13 +0100

    import forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
